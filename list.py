bd88a3c9aa8c75547d3d6adf044d3bb5c9ea8b4e
graendyulianto <graendyulianto@gmail.com>
2026-01-30 10:43:16 +0700

    First Commit

---
+++ list.py
@@ -0,0 +1,10 @@
+mahasiswa = ["Andi", "Budi", "Citra", "Dewi"]
+mahasiswa.append("Eka")
+mahasiswa.insert(1, "Agus")
+mahasiswa[2] = "Bambang"
+mahasiswa.remove("Dewi")
+
+
+print(f"Daftar mahasiswa: {mahasiswa}")
+print(len(mahasiswa))
+
